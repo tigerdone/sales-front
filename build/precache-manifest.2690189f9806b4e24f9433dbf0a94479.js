c9d683afa89ba821dbb5d72db72abbcd7c563391
lihu@codoon.com <lihu@codoon.com>
2025-01-23 19:55:26 +0800

    feat: build

---
+++ build/precache-manifest.2690189f9806b4e24f9433dbf0a94479.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa98bea85bf6e283a03",
+    "url": "/static/js/main.baa98bea.chunk.js"
+  },
+  {
+    "revision": "0271dbedecec80541c64",
+    "url": "/static/css/1.40e6d7a4.chunk.css"
+  },
+  {
+    "revision": "0271dbedecec80541c64",
+    "url": "/static/js/1.0271dbed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7c972351dfb6b4642c8eaa410ff19c5",
+    "url": "/index.html"
+  }
+];
